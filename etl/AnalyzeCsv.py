ebf5d20539d093afc828ff9818baa198fcf071b8
Rene Bouw <rpbouw@gmail.com>
2019-03-18 21:17:37 +0100

    cleanup

--- etl/AnalyzeCsv.py
+++ etl/AnalyzeCsv.py
@@ -13,13 +13,17 @@ def read_csv(csv_file):
                                                           "Wiki Page": "wikiPage",
                                                           "Plot": "plot"})
     print(movies_data_frame.columns)
-    print(movies_data_frame["title"].str.len().max())
-    print(movies_data_frame["originEthnicity"].str.len().max())
-    print(movies_data_frame["director"].str.len().max())
-    print(movies_data_frame["cast"].str.len().max())
-    print(movies_data_frame["genre"].str.len().max())
-    print(movies_data_frame["wikiPage"].str.len().max())
-    print(movies_data_frame["plot"].str.len().max())
+    print_column(movies_data_frame,"title")
+    print_column(movies_data_frame,"originEthnicity")
+    print_column(movies_data_frame,"director")
+    print_column(movies_data_frame,"cast")
+    print_column(movies_data_frame,"genre")
+    print_column(movies_data_frame,"wikiPage")
+    print_column(movies_data_frame,"plot")
+
+
+def print_column(data_frame, column_name):
+    print(column_name, data_frame[column_name].str.len().max())
 
 
 if __name__ == '__main__':
