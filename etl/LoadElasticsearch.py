9ba6a58d1ab2f1ce7a599991b4384ec6ad85e84e
Rene Bouw <rpbouw@gmail.com>
2019-03-23 13:58:36 +0100

    cleanup before refactor

--- etl/LoadElasticsearch.py
+++ etl/LoadElasticsearch.py
@@ -1,8 +1,18 @@
 import csv
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 
 
-def map_csv_to_elasticsearch_model(csv_model):
+def copy_csv_to_elasticsearch(csv_file):
+    elasticsearch = Elasticsearch()
+
+    with open(csv_file, encoding='UTF-8') as f:
+        for csv_model in csv.DictReader(f, skipinitialspace=True):
+            elasticsearch_model = __map_csv_to_elasticsearch_model(csv_model)
+            elasticsearch.index(index="movies", doc_type='_doc', body=elasticsearch_model)
+
+
+def __map_csv_to_elasticsearch_model(csv_model):
     return {'releaseYear': csv_model['Release Year'],
             'title': csv_model['Title'],
             'originEthnicity': csv_model['Origin/Ethnicity'],
@@ -11,15 +21,6 @@ def map_csv_to_elasticsearch_model(csv_model):
             'genre': csv_model['Genre'],
             'wikiPage': csv_model['Wiki Page'],
             'plot': csv_model['Plot']}
-
-
-def copy_csv_to_elasticsearch(csv_file):
-    elasticsearch = Elasticsearch()
-
-    with open(csv_file, encoding='UTF-8') as f:
-        for csv_model in csv.DictReader(f, skipinitialspace=True):
-            elasticsearch_model = map_csv_to_elasticsearch_model(csv_model)
-            elasticsearch.index(index="movies", doc_type='_doc', body=elasticsearch_model)
 
 
 if __name__ == '__main__':
