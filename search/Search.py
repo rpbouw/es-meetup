00ed0dfa6169922930a13e40ccca88cf6a227486
Rene Bouw <rpbouw@gmail.com>
2019-04-14 21:16:46 +0200

    added website

---
+++ search/Search.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template
+from elasticsearch import Elasticsearch
+
+elasticsearch = Elasticsearch()
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    keyword = request.form['keyword']
+
+    if keyword:
+        body = {
+            "query": {
+                "match": {
+                    "cast": keyword
+                }
+            }
+        }
+    else:
+        body = {
+            "query": {
+                "match_all": {}
+            }
+        }
+
+    search_result = elasticsearch.search(index="movies", body=body)
+    number_of_hits = search_result['hits']['total']['value']
+
+    return render_template('index.html', keyword=keyword, hits=number_of_hits, movies=search_result['hits']['hits'])
+
+
+app.run(port=5000, debug=True)
