d0807f27092752d1fcfed0aac3617677b6f208e0
Rene Bouw <rpbouw@gmail.com>
2019-04-16 21:38:51 +0200

    added more like this

--- search/Search.py
+++ search/Search.py
@@ -35,4 +35,38 @@ def search():
     return render_template('index.html', keyword=keyword, hits=number_of_hits, movies=search_result['hits']['hits'])
 
 
+@app.route('/morelikethis', methods=['GET'])
+def more_like_this():
+    id = request.args['id']
+    keyword = request.args['keyword']
+
+    if id:
+        body = {
+            "query": {
+                "more_like_this": {
+                    "fields": [
+                        "plot.simple"
+                    ],
+                    "like": [
+                        {
+                            "_index": "movies",
+                            "_id": id
+                        }
+                    ],
+                    "min_term_freq": 1,
+                    "max_query_terms": 30
+                }
+            }
+        }
+    else:
+        body = {
+            "query": {
+                "match_all": {}
+            }
+        }
+
+    search_result = elasticsearch.search(index="movies", body=body)
+    number_of_hits = search_result['hits']['total']['value']
+    return render_template('index.html', keyword=keyword, hits=number_of_hits, movies=search_result['hits']['hits'])
+
 app.run(port=5000, debug=True)
